0505990ddfb458222df74cf68d398a94865cf124
Vincenzo Mezzio <vgmezzio@gmail.com>
2021-01-13 17:13:12 -0500

    Updated the banner with a gif, making sure searchfield has correct code

--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -1,21 +1,71 @@
-import React,{Component} from 'react'
+import React, {Component} from 'react'
+import GifCard from './GifCard';
 
-export default class SearchField extends React.Component {
+export default class SearchField extends Component {
+
+    constructor(){
+        super();
+        this.state = {
+            gifName: null,
+            gifData: [],
+        };
+    }
+
+    //every time a user enters a gif name, update it
+    handleChange = event => {
+        console.log("inside handleChange")
+        this.setState({gifName: event.target.value});
+    }
+
+
+    //get data from an api
+    getData = async () => {
+        console.log("inside getData")
+        try{
+            //get a response from an api
+            let response = await fetch("http://api.giphy.com/v1/gifs/search?q="+this.state.gifName+"&api_key=W8fWbmNISlnnYSoZC6fsnCxrmYHlodt8");
+            
+            //if there was an error, print it
+            if(!response.ok){
+                throw new Error("Something went wrong");
+            }
+            
+            //turn the response into a data object
+            let data = await response.json();
+
+            this.setState({
+                gifData: data, //set the city data to the data received from an api
+            });
+            console.log(this.state.gifData);
+        }
+
+        catch(error){
+            console.log(error);
+            this.setState({
+                gifData: []
+            });
+        }
+    }    
+
 
     render(){
         return (
             <div>
-                <form>
-                    <input
-                        type="text"
-                        name="searchInput"
-                        placeholder="Search for a GIF!"
-                    >
-                    </input> 
-                    <input
-                        type="submit">
-                    </input>
-                </form>
+                <input
+                    type="text"
+                    name="searchInput"
+                    placeholder="Search for a gif!"
+                    onChange={this.handleChange}
+                >
+                </input>
+
+                <button
+                    id="search-btn"
+                    onClick={() => {this.getData()}}
+                >
+                    Search
+                </button>
+
             </div>
         )
     }
